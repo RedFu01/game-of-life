e052b307858ef9bb9a3e14f948ab46a98f669bb1
RedFu01 <konradfuger@googlemail.com>
2019-02-27 14:08:03 +0100

    progress

--- src/Game.js
+++ src/Game.js
@@ -4,24 +4,66 @@ export default class Game {
     }
 
     start() {
-        const gameStateCopy = JSON.parse(JSON.stringify(this.gameState));
-
-        const iterationCount = 0;
-
-        while (true) {
-            iterationCount++;
-        }
-
-
-
         const sum = arr => arr.reduce((res, curr) => res + curr, 0);
         const flatten = arr => arr.reduce((res, curr) => [...res, ...curr], []);
 
-        const dataSum = sum(flatten(this.gameState));
-        const iterationCount = dataSum;
+        let iterationCount = 0;
+        while (true) {
+            if (sum(flatten(this.gameState)) === 0) {
+                break;
+            }
+
+            this.gameState = this.createNextState();
+
+            iterationCount++;
+        }
 
         return {
             iterationCount
         };
+    }
+
+    static getNeighbours(gameState, x, y) {
+        const neighbours = [];
+        for (let i = 0; i < gameState.length; i++) {
+            for (let j = 0; j < gameState[i].length; j++) {
+                if (Math.abs(x - i) <= 1 && Math.abs(y - j) <= 1) {
+                    if (!(x === i && y === j)) {
+                        neighbours.push(gameState[i][j]);
+                    }
+                }
+            }
+        }
+
+        return neighbours;
+    }
+
+    static getAliveNeighbourCount(gameState, x, y) {
+        const neighbours = Game.getNeighbours(gameState, x, y);
+        const aliveNeighbours = neighbours.filter(n => n === 1).length;
+        return aliveNeighbours;
+    }
+
+    static getNextCellState(gameState, i, j) {
+        const currentState = gameState[i][j];
+
+        if (currentState === 1) {
+            return Number([2, 3].includes(Game.getAliveNeighbourCount(gameState, i, j)));
+        } else {
+            return Number(Game.getAliveNeighbourCount(gameState, i, j) === 3);
+        }
+    }
+
+    createNextState() {
+        const gameStateCopy = JSON.parse(JSON.stringify(this.gameState));
+
+        for (let i = 0; i < gameStateCopy.length; i++) {
+            for (let j = 0; j < gameStateCopy[i].length; j++) {
+
+                gameStateCopy[i][j] = Game.getNextCellState(this.gameState, i, j);
+            }
+        }
+
+        return gameStateCopy;
     }
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,5 @@
-import gameOfLife from '../src/gameOfLife.js'
+import gameOfLife from '../src/gameOfLife.js';
+import Game from '../src/Game';
 
 test('Test that the saved game file is properly read', () => {
     let output = '';
@@ -26,4 +27,41 @@ test('Test game with just one life is over on the second iteration', () => {
 test('Test game with two spread life is over on the second iteration', () => {
     const gameSummary = gameOfLife('./data/002-in-4x4.json');
     expect(gameSummary.iterationCount).toBe(1);
+});
+
+test('Test game with three spread life is over on third iteration', () => {
+    const gameSummary = gameOfLife('./data/003-in-4x4.json');
+    expect(gameSummary.iterationCount).toBe(2);
+});
+
+test('Test next cell state is dead if only two neighbours', () => {
+
+    const state = [
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 1, 0]
+    ];
+
+    const cellState = Game.getNextCellState(state, 1,1);
+
+    expect(cellState).toBe(0);
+});
+
+test('Test cell survives if 3 neighbours', () => {
+
+    const state = [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 1, 0]
+    ];
+
+    const cellState = Game.getNextCellState(state, 1,1);
+
+    expect(cellState).toBe(1);
+});
+
+
+test('Test game with maxIteration', () => {
+    const gameSummary = gameOfLife('./data/infinite-in-5x5.json');
+    expect(gameSummary.iterationCount).toBe(2);
 });
